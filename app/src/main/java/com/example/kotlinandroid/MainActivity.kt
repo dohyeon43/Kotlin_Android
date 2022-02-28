4c8c80ad7b0c91ffc8a484e8313e933f39b734f7
{kdh1123} <{kdh200533@gmail.com}>
2022-02-28 17:52:34 +0900

    메인 액티비티 -> 액티비티1/2/3/4/5/6로 전환 기능 (임시)

--- app/src/main/java/com/example/kotlinandroid/MainActivity.kt
+++ app/src/main/java/com/example/kotlinandroid/MainActivity.kt
@@ -3,19 +3,24 @@ package com.example.kotlinandroid
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintSet
 import com.example.kotlinandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //뷰 바인딩
+
         //자동으로 만들어지는 클래스의 이름은 레이아웃 xml 파일 명을 따름름
        val binding = ActivityMainBinding.inflate(layoutInflater) //binding 객체 호출
         setContentView(binding.root)
 
-        binding.activity1.setOnClickListener(){
+        binding.activity1.setOnClickListener(){ //임시 액티비티 전환 버튼
             val intent = Intent(this,Activity1::class.java)
             startActivity(intent)
         }
@@ -40,5 +45,20 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        when (event?.action){
+            MotionEvent.ACTION_MOVE -> {
+
+            }
+            MotionEvent.ACTION_UP ->{
+
+            }
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return super.onTouchEvent(event)
     }
 }
