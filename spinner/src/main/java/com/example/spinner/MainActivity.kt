0189bae9f833769142957c63029efaf1476f0ede
{kdh1123} <81676485+kdh1123@users.noreply.github.com>
2022-03-21 18:20:01 +0900

    Add Spinner

--- spinner/src/main/java/com/example/spinner/MainActivity.kt
+++ spinner/src/main/java/com/example/spinner/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.spinner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val spinner : Spinner = findViewById(R.id.my_spinner)
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.my_array,
+            R.layout.activity_main
+        ).also { adapter ->
+            spinner.onItemSelectedListener
+            spinner.adapter = adapter
+        }
     }
 }
