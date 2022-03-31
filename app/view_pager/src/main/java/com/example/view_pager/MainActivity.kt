e8500d53f5dc67f2f77f2a58fba27d854061c36b
{kdh1123} <81676485+kdh1123@users.noreply.github.com>
2022-03-31 21:03:22 +0900

    Add .

--- app/view_pager/src/main/java/com/example/view_pager/MainActivity.kt
+++ app/view_pager/src/main/java/com/example/view_pager/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //오늘 커밋 이걸로 하겠습니다 죄송합니다.
     }
 }
