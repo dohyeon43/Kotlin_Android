4d99278b34a5373592d44f8329e339ceaab85f05
{kdh1123} <81676485+kdh1123@users.noreply.github.com>
2022-04-24 21:45:36 +0900

    Add : MVVM 모듈에 retrofit 객체 생성
    2022-04-24

--- MVVM/src/main/java/com/example/android_kotlin/MainActivity.kt
+++ MVVM/src/main/java/com/example/android_kotlin/MainActivity.kt
@@ -6,15 +6,19 @@ import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import com.example.android_kotlin.databinding.ActivityMainBinding
 import com.example.android_kotlin.viewModel.MainViewModel
+import com.google.gson.Gson
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class MainActivity : AppCompatActivity() {
     lateinit var mBinding : ActivityMainBinding
     val model: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
+        //retrofit 객체 생성
+        val retrofit = Retrofit.Builder().baseUrl("").addConverterFactory(GsonConverterFactory.create()).build()
         super.onCreate(savedInstanceState)
         mBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
-
         mBinding.lifecycleOwner = this
         mBinding.viewModel = model
     }
