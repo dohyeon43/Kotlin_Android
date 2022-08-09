188f148a5759dee6176d310d3035918d511ad274
{kdh1123} <81676485+kdh1123@users.noreply.github.com>
2022-08-09 15:01:27 +0900

    FIx : Error
    2022-08-09

--- room/src/main/java/com/example/room/MainActivity.kt
+++ room/src/main/java/com/example/room/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.room
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
 import com.example.room.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,21 +16,16 @@ class MainActivity : AppCompatActivity() {
         val binding : ActivityMainBinding
                 = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
-        val db = StudentDatabase.getInstance(this)
-        //DB 객체 생성
-
-        val list = db!!.studentDao().getAll()
         val mainAdapter = MainAdapter()
+        val db = StudentDatabase.getInstance(this)
 
         binding.recyclerView.apply {
             adapter = mainAdapter
             layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL,false)
         }
+        val list = db!!.studentDao().getAll()
         mainAdapter.submitList(list)
 
-
-
-        //Button을 누를 시, DB에 학생 정보를 넣어주는 코드
         binding.btnSubmit.setOnClickListener(View.OnClickListener {
             db.studentDao().addStudent(
                 binding.name.text.toString(),
@@ -37,5 +34,6 @@ class MainActivity : AppCompatActivity() {
                 binding.stuNum.text.toString().toInt()
             )
         })
+
     }
 }
--- room/src/main/java/com/example/room/StudentDatabase.kt
+++ room/src/main/java/com/example/room/StudentDatabase.kt
@@ -10,19 +10,17 @@ abstract class StudentDatabase : RoomDatabase(){
     abstract fun studentDao() : StudentDao
 
 
-//싱글톤 패턴 적용
     companion object {
         private var instance: StudentDatabase? = null
-
-        @Synchronized
         fun getInstance(context: Context): StudentDatabase? {
             if (instance == null) {
                 synchronized(StudentDatabase::class){
                     instance = Room.databaseBuilder(
                         context.applicationContext,
                         StudentDatabase::class.java,
-                        "user-database"
-                    ).build()
+                        "student_database"
+                    ).allowMainThreadQueries()
+                        .build()
                 }
             }
             return instance
