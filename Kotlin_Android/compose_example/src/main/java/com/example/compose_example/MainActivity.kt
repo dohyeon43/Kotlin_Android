4a414561954bfa8019d645855f48de8a7737e928
DoHyeon <kdh200533@gmail.com>
2023-01-13 23:19:43 +0900

    Add : @Composable, @Preview 사용해보기
    2023-01-13

--- Kotlin_Android/compose_example/src/main/java/com/example/compose_example/MainActivity.kt
+++ Kotlin_Android/compose_example/src/main/java/com/example/compose_example/MainActivity.kt
@@ -37,13 +37,6 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Composable
-fun AffirmationCard(affectingSpan: MetricAffectingSpan, modifier: Modifier){
-    Card(modifier = Modifier){
-
-    }
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
